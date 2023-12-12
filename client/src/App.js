0a4b4530621ca8b0f6aa6f3a98b2914c671631b2
jyotiraditya mishra <ashujimishra2003@gmail.com>
2023-12-12 09:54:09 +0530

    all setup done in client side

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react'
-
+import {Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-     hello 
-    </div>
+   <>
+   <Routes>
+    <Route path='/' element={<Home/>}/>
+   </Routes>
+   </>
   )
 }
 
