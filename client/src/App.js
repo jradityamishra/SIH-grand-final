c5fc60e977345781052d4976a03ee0956bc34576
meghnadutta02 <meghnadutta02@gmail.com>
2023-12-15 11:15:14 +0530

    mcq generation improved

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
-import React from 'react'
-
+import React from "react";
+import EditorPage from "./EditorPage";
 const App = () => {
   return (
-    <div className='text-3xl font-bold underline'>
-     hello 
+    <div className="text-3xl font-bold underline">
+      hello
+      <EditorPage />
     </div>
-  )
-}
+  );
+};
 
-export default App
+export default App;
